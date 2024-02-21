0f50f0bedd3c437dbb63e13107e1717375fb3bb8
Makaka449 <145019010+Makaka449@users.noreply.github.com>
2024-02-21 14:44:03 +0200

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,24 @@
+function checkForSpam(message) {
+    // Перевіряємо, чи рядок містить заборонені слова
+    const сaseMessage = message.toLowerCase();
+    
+    switch (true) {
+        case сaseMessage.includes("spam"):
+        case сaseMessage.includes("sale"):
+            return true;
+        default:
+            return false;
+    }
+}
+
+
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+
+
+ 
